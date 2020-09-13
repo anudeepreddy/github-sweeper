a63a56fb2eada3a163faffcc14054f11b1ba6f74
Anudeep Reddy <anudeepc85@gmail.com>
2020-09-13 18:59:37 +0530

    moved styled-components to global styles

--- components/Hero.js
+++ components/Hero.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
 import {Box,Heading,Text,Button} from '@chakra-ui/core';
 import {FaLongArrowAltRight} from 'react-icons/fa';
 import { signIn } from 'next-auth/client';
 
-const Flex = styled.div`
-  padding: 1em;
-  display: flex;
-  flex-direction: row;
-  min-height: 90vh;
-  align-items: center;
-  justify-content: space-around;
-  background-image: linear-gradient(to top, #d16ba5, #c777b9, #ba83ca, #aa8fd8, #9a9ae1, #8aa7ec, #79b3f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1);
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
-`
 
 function Hero(){
   return (
-    <Flex>
+    <div className='hero'>
       <Box maxW="32rem">
         <Heading mb={4}>Do you have a lot of Github repos to Delete?</Heading>
         <Text fontSize="xl">
@@ -29,9 +16,8 @@ function Hero(){
           Create a free account
         </Button>
       </Box>
-        <img src="/hero.svg" width="400"/>
-      
-    </Flex>    
+      <img src="/hero.svg" width="400"/>
+    </div>    
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import {Provider} from 'next-auth/client';
 import Nav from '../components/Nav';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
