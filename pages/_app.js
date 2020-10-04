c406ae0390f81741171964aac2fc4647f6d0c490
Anudeep Reddy <anudeepc85@gmail.com>
2020-10-04 13:45:10 +0000

    add buy me a coffee widget

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import {Provider} from 'next-auth/client';
 import Nav from '../components/Nav';
@@ -8,6 +9,9 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
+    <Head>
+      <script data-name="BMC-Widget" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="anudeepc" data-description="Support me on Buy me a coffee!" data-message="Thank you for visiting. You can now buy me a coffee!" data-color="#5F7FFF" data-position="right" data-x_margin="18" data-y_margin="18"></script>  
+    </Head> 
     <ThemeProvider>
       <CSSReset/>
       <Nav/>
